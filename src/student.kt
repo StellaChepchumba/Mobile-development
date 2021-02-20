ad5027dd2836c9e25211e58bd41da728f638c260
stella4545 <stellachepchumbah@gmail.com>
2021-02-20 22:35:54 +0300

    mobile development assignment

---
+++ src/student.kt
@@ -0,0 +1,14 @@
+fun main() {
+   var name:String="anabel angela"
+    var age:Int=22
+    var phoneNumber:String="0700492654"
+    var weight:String="68kg"
+var citizenship:Boolean=true
+    println(name)
+println(age)
+    println(phoneNumber)
+    println(weight)
+    println(citizenship)
+}
+
+
