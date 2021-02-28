2f948fbfb19a2cd56d629d8f97084182e21ba05b
stella4545 <stellachepchumbah@gmail.com>
2021-02-28 11:30:40 +0300

    first commit

--- src/student.kt
+++ src/student.kt
@@ -1,14 +1,25 @@
-fun main() {
-   var name:String="anabel angela"
-    var age:Int=22
-    var phoneNumber:String="0700492654"
-    var weight:String="68kg"
-var citizenship:Boolean=true
+fun main(){
+    var name:String=("Hello Stellah")
     println(name)
-println(age)
-    println(phoneNumber)
-    println(weight)
-    println(citizenship)
+
+    var modulus=(100%20)
+    println(modulus)
+
+    var resu=(10+20+60+40)
+    println(resu)
+
+    var facts:String="Iam a down to earth person and very optimistic"
+    println(facts)
+
 }
+fun modulus(num1:Int,num2:Int) :Int{
+    var modulus=(num1%num2)
+    return(modulus)
 
+    fun addition(num1:Int,num2: Int,num3:Int,num4:Int):Int{
+        var addition=(num1+num2+num3+num4)
+        return addition
 
+    }
+
+}
